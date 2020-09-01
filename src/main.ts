eba356d0891f7f64b89d3e2617e1dc7af3e8489a
ryota <ryota.shunpu.sfz2@gmail.com>
2020-09-01 15:32:40 +0900

    完成

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,16 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+// ---------------↓ここから追記↓---------------
+import Amplify, * as AmplifyModules from 'aws-amplify'
+// @ts-ignore
+import { AmplifyPlugin } from 'aws-amplify-vue'
+// @ts-ignore
+import aws_exports from './aws-exports'
+Amplify.configure(aws_exports)
 
+Vue.use(AmplifyPlugin, AmplifyModules)
+// ---------------↑ここまで追記↑---------------
 Vue.config.productionTip = false
 
 new Vue({
